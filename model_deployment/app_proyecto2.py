24bcf11ab6af3bb62eb4cf08737a3f9a5c22a8b9
cagutig <87836459+cagutig@users.noreply.github.com>
2024-05-26 18:30:02 -0500

    Update app_proyecto2.py

--- model_deployment/app_proyecto2.py
+++ model_deployment/app_proyecto2.py
@@ -1,6 +1,6 @@
 # Importar librerías
 from flask import Flask, request, jsonify
-from flask_restx import Api, Resource, fields, reqparse
+from flask_restx import Api, Resource, fields
 from tensorflow.keras.models import load_model
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
@@ -15,7 +15,7 @@ api = Api(app, version='1.0', title='Movie Genre Classification API',
 ns = api.namespace('predict', description='Predicción del modelo')
 
 # Definir el modelo de datos esperado
-model = api.model('PredictionData', {
+model_input = api.model('PredictionData', {
     'title': fields.String(required=True, description='Título de la película'),
     'plot': fields.String(required=True, description='Sinopsis de la película'),
 })
@@ -30,20 +30,15 @@ vectorizer_path = 'tfidf_vectorizer.pkl'
 with open(vectorizer_path, 'rb') as file:
     vectorizer = pickle.load(file)
 
-# Definir el parser para los argumentos
-parser = reqparse.RequestParser()
-parser.add_argument('title', type=str, required=True, help='Título de la película')
-parser.add_argument('plot', type=str, required=True, help='Sinopsis de la película')
-
 # Definir la clase para la predicción
 @ns.route('/')
 class MovieGenreApi(Resource):
-    @api.expect(model)
+    @api.expect(model_input)
     @api.response(200, 'Success')
     def post(self):
-        args = parser.parse_args()
-        title = args['title']
-        plot = args['plot']
+        data = request.get_json(force=True)
+        title = data['title']
+        plot = data['plot']
         title_plot = f"{title} {plot}"
         
         # Vectorizar el texto usando el vectorizador TF-IDF
@@ -64,3 +59,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+
