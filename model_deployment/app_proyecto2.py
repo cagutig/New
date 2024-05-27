c2a6c8858b82379c69cee0903922c7ed7e26e776
cagutig <87836459+cagutig@users.noreply.github.com>
2024-05-26 20:08:40 -0500

    Update app_proyecto2.py

--- model_deployment/app_proyecto2.py
+++ model_deployment/app_proyecto2.py
@@ -1,4 +1,3 @@
-# Importar librerías
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from tensorflow.keras.models import load_model
@@ -7,7 +6,7 @@ import numpy as np
 import pickle
 
 # Inicializar Flask y Flask-RESTX
-app = Flask(_name_)
+app = Flask(__name__)
 api = Api(app, version='1.0', title='Movie Genre Classification API',
           description='API para clasificar géneros de películas basado en el título y la sinopsis.')
 
@@ -30,7 +29,6 @@ vectorizer_path = 'tfidf_vectorizer.pkl'
 with open(vectorizer_path, 'rb') as file:
     vectorizer = pickle.load(file)
 
-# Definir la clase para la predicción
 @ns.route('/')
 class MovieGenreApi(Resource):
     @api.expect(model_input)
@@ -41,22 +39,12 @@ class MovieGenreApi(Resource):
         plot = data['plot']
         title_plot = f"{title} {plot}"
         
-        # Vectorizar el texto usando el vectorizador TF-IDF
         input_data = vectorizer.transform([title_plot]).toarray()
-        
-        # Hacer la predicción
         prediction = model.predict(input_data)
-        
-        # Convertir la predicción a una lista para que sea serializable en JSON
         prediction = prediction.tolist()
         
         return jsonify({'prediction': prediction})
 
-# Ruta para verificar el funcionamiento de la API
-@app.route('/')
-def home():
-    return "La API está funcionando correctamente."
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
 
