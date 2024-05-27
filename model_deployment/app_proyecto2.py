8889edf16202f5afb7aec4e1f31a0f01ca0e7ce9
cagutig <87836459+cagutig@users.noreply.github.com>
2024-05-26 20:03:44 -0500

    Update app_proyecto2.py

--- model_deployment/app_proyecto2.py
+++ model_deployment/app_proyecto2.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, render_template
+# Importar librerías
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from tensorflow.keras.models import load_model
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -6,9 +7,9 @@ import numpy as np
 import pickle
 
 # Inicializar Flask y Flask-RESTX
-app = Flask(__name__, template_folder='templates')
+app = Flask(_name_)
 api = Api(app, version='1.0', title='Movie Genre Classification API',
-          description='API para clasificar géneros de películas basado en el título y la sinopsis.', doc='/docs')
+          description='API para clasificar géneros de películas basado en el título y la sinopsis.')
 
 # Definir el namespace
 ns = api.namespace('predict', description='Predicción del modelo')
@@ -20,7 +21,7 @@ model_input = api.model('PredictionData', {
 })
 
 # Cargar el modelo de red neuronal entrenado y recompilarlo
-model_path = './corrected_model.h5'
+model_path = 'corrected_model.h5'
 model = load_model(model_path)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['AUC'])
 
@@ -29,6 +30,7 @@ vectorizer_path = 'tfidf_vectorizer.pkl'
 with open(vectorizer_path, 'rb') as file:
     vectorizer = pickle.load(file)
 
+# Definir la clase para la predicción
 @ns.route('/')
 class MovieGenreApi(Resource):
     @api.expect(model_input)
@@ -38,17 +40,23 @@ class MovieGenreApi(Resource):
         title = data['title']
         plot = data['plot']
         title_plot = f"{title} {plot}"
-
+        
+        # Vectorizar el texto usando el vectorizador TF-IDF
         input_data = vectorizer.transform([title_plot]).toarray()
+        
+        # Hacer la predicción
         prediction = model.predict(input_data)
+        
+        # Convertir la predicción a una lista para que sea serializable en JSON
         prediction = prediction.tolist()
-
+        
         return jsonify({'prediction': prediction})
 
+# Ruta para verificar el funcionamiento de la API
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "La API está funcionando correctamente."
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
 
